0ef50175c1c808683fa3f50cc859f53238bb452c
Андрей Мирский <andrey.riwall@gmail.com>
2021-07-05 23:50:52 +0300

    2

---
+++ js/main.js
@@ -0,0 +1,61 @@
+   // bg
+   gsap.to("#bg", {
+    scrollTrigger : {
+      scrub : 1
+    },
+    scale : 2
+  });
+  
+  
+  // man
+  gsap.to("#man", {
+    scrollTrigger : {
+      scrub : 1
+    },
+    scale : 0.3
+  });
+  
+  
+  // clouds_1
+  gsap.to("#clouds_1", {
+    scrollTrigger : {
+      scrub : 1
+    },
+    x : 400,
+  });
+  
+  
+  // clouds_2
+  gsap.to("#clouds_2", {
+    scrollTrigger : {
+      scrub : 1
+    },
+    x : -400,
+  });
+  
+  
+  // mountains_left
+  gsap.to("#mountain_left", {
+    scrollTrigger : {
+      scrub : 1
+    },
+    x : -800,
+  });
+  
+  
+  // mountains_right
+  gsap.to("#mountain_right", {
+    scrollTrigger : {
+      scrub : 1
+    },
+    x : 800,
+  });
+  
+  
+  // text
+  gsap.to("#text", {
+    scrollTrigger : {
+      scrub : 1
+    },
+    y : 950,
+  });
